17f92aaf5fdc5a7f3cb3be21228b52241a9810ef
Alok Hegde <hegdealok@yahoo.com>
2022-08-26 20:29:31 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,63 @@
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+const helmet = require("helmet");
+const mongoose = require("mongoose");
+const https = require("https");
+const fs = require("fs");
+
+const options = {
+  key: fs.readFileSync("key.pem"),
+  cert: fs.readFileSync("cert.pem"),
+};
+
+//importing dot env
+require("dotenv/config");
+
+//initializing api
+//which is the initial route of api
+const api = process.env.API_URL;
+
+//Initializing app
+const app = express();
+
+//CORS
+app.use(cors());
+app.options("*", cors());
+
+//Middlewares
+//Middleware to serve static files
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(morgan("tiny"));
+//Always use helmet for safety
+app.use(helmet());
+
+// ROUTE MIDDLEWARE IMPORTS
+const adminRoute = require("./routes/admin/admin");
+
+// ROUTE MIDDLEWARES
+app.use(`${api}/admin`, adminRoute);
+
+//Connecting to mongodb database
+mongoose
+  .connect(process.env.DEV_DATABASE + "/instant_gamez", {
+    useNewUrlParser: true,
+  })
+  .then(() => {
+    console.log("Database connection is ready");
+  })
+  .catch((err) => {
+    console.error(err);
+  });
+
+//Initializing port
+const port = process.env.PORT || 3000;
+
+var server = https.createServer(options, app);
+
+//Running server
+server.listen(port, () => {
+  console.log(`Server is running at port ${port} ...`);
+});
