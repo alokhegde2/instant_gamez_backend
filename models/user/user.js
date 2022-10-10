38f7127d494650a94f9b6e9d0c529fee5245d35d
Alok Hegde <hegdealok@yahoo.com>
2022-10-10 22:46:08 +0530

    Fixed registeration issue

--- models/user/user.js
+++ models/user/user.js
@@ -39,7 +39,7 @@ const userSchema = new mongoose.Schema({
     type: mongoose.Schema.Types.ObjectId,
     required: false,
     ref: "wallets",
-    default: "",
+    default: null,
   },
   createdDate: {
     type: Date,
