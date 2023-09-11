3b0c547ed654ec7b8759eeb1e519a97e32c4c128
Alok Hegde <hegdealok@yahoo.com>
2023-09-11 22:34:28 +0530

    Added Withdraw logic

--- routes/wallet/transaction.js
+++ routes/wallet/transaction.js
@@ -11,6 +11,7 @@ require("dotenv/config");
 
 //IMPORTING MODEL
 const Transactions = require("../../models/wallet/transaction");
+const Withdraw = require("../../models/wallet/withdrawalRequest");
 
 //Getting the transactions of the specific user
 app.get("/:id", verify.verify, async (req, res) => {
@@ -40,7 +41,6 @@ app.get("/:id", verify.verify, async (req, res) => {
       var transaction = await Transactions.find({
         user: id,
         $or: [
-          { typeOfTransaction: "Withdraw" },
           { typeOfTransaction: "Deposit" },
         ],
       }).sort({ dateOfTransaction: -1 });
@@ -53,6 +53,44 @@ app.get("/:id", verify.verify, async (req, res) => {
           { typeOfTransaction: "GamePlay" },
         ],
       }).sort({ dateOfTransaction: -1 });
+    } else if (transType == "W") {
+      const status = true;
+      var transaction = await Withdraw.find({
+        user: id,
+      })
+        .select({
+          user: "$userId",
+          wallet: "$walletId",
+          amountOfTransaction: "$amount",
+          typeOfTransaction: "Withdraw",
+          dateOfTransaction: "$updatedAt",
+          message: "$description",
+          createdDate: "$createdAt",
+          id:"$_id",
+          status: {
+            $cond: {
+              if: { $eq: ["$isApprove", 0] },
+              then: "Pending",
+              else: {
+                $cond: {
+                  if: { $eq: ["$isApprove", 2] },
+                  then: "Approved",
+                  else: "Rejected",
+                },
+              },
+            },
+          },
+        })
+        .sort({ createdAt: -1 });
+
+      // Manually add a hardcoded boolean value to each document
+      const resultsWithHardcodedValue = transaction.map((doc) => ({
+        ...doc.toObject(),
+        isSuccess: true,
+        __v: 0, // Change this to your desired boolean value
+      }));
+
+      transaction = resultsWithHardcodedValue;
     } else {
       var transaction = await Transactions.find({
         user: id,
